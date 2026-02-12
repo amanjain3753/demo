b44b659b9a3ff431069f0f7568fdf8e522d662a2
amanjain3753 <134941840+amanjain3753@users.noreply.github.com>
2026-02-12 14:51:40 +0530

    Update silver.py

--- src/notebook/silver.py
+++ src/notebook/silver.py
@@ -11,7 +11,7 @@ df_clean = (
       .withColumn("SALES", col("SALES").cast("double"))
       .withColumn("QUANTITYORDERED", col("QUANTITYORDERED").cast("int"))
       .withColumn("PRICEEACH", col("PRICEEACH").cast("double"))
-      .withColumn("ORDERDATE", to_timestamp("dob", "M/d/yyyy H:mm")
+      .withColumn("ORDERDATE", to_timestamp(col("ORDERDATE"), "M/d/yyyy H:mm")
 )
 
 )
